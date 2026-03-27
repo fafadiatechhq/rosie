d7fa67e512f25fc566319e622b77ed4808ab9af4
Teesha Vaza <teesha.vaza@fafadiatech.com>
2026-03-27 12:26:24 +0530

    ref #31 : Implemented Admin Retention page

---
+++ frontend/rosie/app/admin/retention/page.tsx
@@ -0,0 +1,12 @@
+import RetentionContainer from '@/components/admin/RetentionContainer'
+import React from 'react'
+
+const RetentionPage = () => {
+  return (
+    <React.Fragment>
+      <RetentionContainer />
+    </React.Fragment>
+  )
+}
+
+export default RetentionPage
