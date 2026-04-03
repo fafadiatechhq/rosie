d3adf8f2a0d6b37fd937affb8e5807b24d30c0a2
sarvesh.devrukhkar <sarvesh.devrukhkar@fafadiatech.com>
2026-04-03 11:02:08 +0530

    ref #27 - Implemented 'Admin/Accounts' page.
    Changes:
    - Implemented `admin/accounts` page.
    - Export reusable function from `/utils`.

---
+++ frontend/rosie/app/admin/accounts/page.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from "@/components/admin/AdminLayout";
+import AccountsContainer from "@/components/admin/AccountsContainer";
+
+const AccountsPage = () => {
+  return (
+    <AdminLayout>
+      <AccountsContainer />
+    </AdminLayout>
+  );
+};
+
+export default AccountsPage;
--- frontend/rosie/utils/formatNumber.ts
+++ frontend/rosie/utils/formatNumber.ts
@@ -1,5 +1,5 @@
- export const formatNumber = (n: number): string => {
+export const formatNumber = (n: number): string => {
   if (n >= 1_000_000) return (n / 1_000_000).toFixed(1) + "M";
   if (n >= 1_000) return (n / 1_000).toFixed(1) + "K";
   return n.toString();
-}
+};
