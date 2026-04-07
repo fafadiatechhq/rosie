1671cd826edd7d1c9288b31f20e4b44d6e82988a
Gauri Patil <gauri.patil@fafadiatech.com>
2026-04-07 12:07:47 +0530

    ref #39 : implement admin global logs page

---
+++ frontend/rosie/app/admin/logs/page.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from '@/components/admin/AdminLayout'
+import { GlobalLogsContainer } from '@/components/admin/GlobalLogsContainer'
+
+const RetentionPage = () => {
+  return (
+    <AdminLayout>
+      <GlobalLogsContainer/>
+    </AdminLayout>
+  )
+}
+
+export default RetentionPage
