b32029a988e37df96c12d89ef1109db25d6d638f
Sidharth Shah <sidharth@fafadiatech.com>
2026-08-08 11:19:11 +0530

    updated setting for local development

--- backend/rosie/rosie/settings.py
+++ backend/rosie/rosie/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-1%!a+us*b^1u+280bm%$ckzl&ny%-g&r_uju$60&8rvyl9+o^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
